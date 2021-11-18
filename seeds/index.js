e7d1041171b034c2ffc07d99c60b06ddcc54701e
Marco <mlorenzetti19@gmail.com>
2021-11-18 21:36:00 +0100

    updated seeds file with Atlas connection

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-//const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 const cities = require('cities.json');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     //useCreateIndex: true,
     useUnifiedTopology: true
